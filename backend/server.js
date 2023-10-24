39c6e633ddca3b0568a726e70f8eabb93f3ba804
naveenG1996 <gnaveenkumar9121996@gmail.com>
2023-10-24 16:45:16 +0530

    Minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
-const express=require('express');
-
-const app=express()
-
-const port=process.env.PORT || 4000
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3500
 
 app.get("/",(req,res)=>{
-    res.send("hello dialpad");
+    res.send("Hello dialpad");
 })
 
 app.listen(port,()=>{
